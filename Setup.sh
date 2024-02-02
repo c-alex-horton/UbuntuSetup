ceb9e6f2f0c3d9ee7962525d23c413e86bd2af5b
Caleb Horton <c.alex.horton@gmail.com>
2024-02-02 16:29:57 -0600

    Removed redundant apt-get line

--- Setup.sh
+++ Setup.sh
@@ -17,8 +17,6 @@ while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
 EOF
 )
 
-sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-
 git clone https://github.com/neovim/neovim.git
 cd neovim
 make CMAKE_BUILD_TYPE=RelWithDebInfo
