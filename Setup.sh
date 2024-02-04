0a006487e28f56f8db6429f9264194f823adf4d5
Caleb Horton <c.alex.horton@gmail.com>
2024-02-04 16:29:22 -0600

    readding sudo

--- Setup.sh
+++ Setup.sh
@@ -3,8 +3,8 @@ USRHOME="$(getent passwd $SUDO_USER | cut -d: -f6)"
 set -eu -o pipefail # fail on error and report it, debug all lines
 
 echo $USRHOME
-# sudo -n true
-# test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+ sudo -n true
+ test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
 # echo updating...
 # apt update
 echo installing the must-have pre-requisites
