d6d0a65573dbbb3fa35000d1012835c5c4fc1981
Caleb Horton <c.alex.horton@gmail.com>
2024-02-04 13:24:51 -0600

    changed how sudo works in script

--- Setup.sh
+++ Setup.sh
@@ -2,10 +2,10 @@
 USRHOME="$(getent passwd $SUDO_USER | cut -d: -f6)"
 set -eu -o pipefail # fail on error and report it, debug all lines
 
-sudo -n true
-test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
-echo updating...
-apt update
+# sudo -n true
+# test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+# echo updating...
+# apt update
 echo installing the must-have pre-requisites
 while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
         ninja-build
@@ -19,12 +19,15 @@ while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
       	neofetch
         golang-go
         luarocks
+        python3.10-venv
 EOF
 )
 
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
+source ~/.bashrc  
 
+nvm install node
 
 git clone https://github.com/neovim/neovim.git
 cd neovim
@@ -38,7 +41,7 @@ echo creating config if it does not exist
 mkdir -p $USRHOME/.config/nvim/
 
 echo getting NeoVim config
-git clone https://github.com/c-alex-horton/kickstart.nvim.git $USRHOME/.config/nvim/
+git clone https://github.com/c-alex-horton/calex-nvim-config.git  $USRHOME/.config/nvim/
 
 sudo chown -R $USER $USRHOME/
 
